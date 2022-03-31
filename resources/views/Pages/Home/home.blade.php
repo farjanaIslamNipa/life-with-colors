cbc5ccb9213882277a200089a8a97ca82dd64a1b
farjana01 <nipa.farzana87@gmail.com>
2022-03-31 21:10:12 +0600

    abc

--- resources/views/Pages/Home/home.blade.php
+++ resources/views/Pages/Home/home.blade.php
@@ -5,7 +5,7 @@ Life With Colors | Home
 
 @section('content')
 <div>
-    <header class="header-banner absolute -top-10 left-0 right-0 bottom-0 z-0 flex items-center justify-center">
+    <header class="header-banner relative -top-32 left-0 right-0 bottom-0 z-0 flex items-center justify-center">
 
         <div class="">
             <h1 class="text-8xl font-semibold"><span class="text-red-500">Life</span><span class="text-teal-600">With</span><span class="text-purple-700">Colors</span></h1>
